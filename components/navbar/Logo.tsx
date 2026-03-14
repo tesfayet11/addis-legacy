da332f285e42fc3cfdd8eb4b0888ecd0f51948cc
tweldegiorgis <tweldegiorgis@k12.com>
2026-03-14 14:57:24 -0400

    initial app

---
+++ components/navbar/Logo.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { VscCode } from 'react-icons/vsc';
+
+function Logo() {
+  return (
+    <Button size='medium' asChild>
+        <Link href='/'>
+            {/*<VscCode className='w-6 h-6'/>*/}
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 200 80"
+                width="300"
+                height="80"
+            >
+                <text
+                    x="100"
+                    y="40"
+                    text-anchor="middle"
+                    dominant-baseline="middle"
+                    font-family="Segoe UI, Roboto, Helvetica, Arial, sans-serif"
+                    font-size="28"
+                    font-weight="600"
+                    fill="#fff"
+
+                >
+                    Legacy Furniture
+                </text>
+            </svg>
+
+
+        </Link>
+    </Button>
+  );
+}
+
+export default Logo;
